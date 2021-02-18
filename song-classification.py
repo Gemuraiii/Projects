e4534cf66c5de57077b192a9d3a3f1c22cd2ea88
Gemuraiii <60904664+Gemuraiii@users.noreply.github.com>
2021-02-18 00:11:49 -0500

    Add files via upload

---
+++ song-classification.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.model_selection import KFold, cross_val_score
