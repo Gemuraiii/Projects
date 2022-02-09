4937ef96eab7ed56b5783d2c870c83a2db37536d
Gemuraiii <60904664+Gemuraiii@users.noreply.github.com>
2022-02-08 20:40:52 -0500

    Delete song-classification.py

--- song-classification.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-import numpy as np 
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
-from sklearn.model_selection import KFold, cross_val_score
